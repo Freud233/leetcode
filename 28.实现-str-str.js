5473269f15076c7e6705e54aa36a471b3f196e69
Xiang Zhang <xiang4603@gmail.com>
2020-02-18 23:13:44 +0800

    完成函数

--- 28.实现-str-str.js
+++ 28.实现-str-str.js
@@ -10,8 +10,20 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    
-};
+var strStr = function (haystack, needle) {
+  if (haystack == needle || needle == '') return 0
+  if (needle.length > haystack.length) return -1
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] == needle[0]) {
+      for (let j = 0; j < needle.length; j++) {
+        if (haystack[i + j] != needle[j]) {
+          break;
+        }
+        if (j + 1 === needle.length) return i
+      }
+    }
+  };
+  return -1;
+}
 // @lc code=end
 
