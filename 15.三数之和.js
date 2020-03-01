b27c998afc66f37ec411ceb1cfb17d122cd3a037
Xiang Zhang <xiang4603@gmail.com>
2020-03-02 06:55:53 +0800

    三数之和未完成版

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -9,8 +9,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-
+var threeSum = function (nums) {
+  // 如果长度小于 3 返回 []
+  if (nums.length < 3) return []
+  // 1. nums 排序
+  nums.sort()
+  let arr = []
+  for (let i = 0; i < nums.length; i++) {
+    let l = i + 1; r = nums.length - 1 - i;
+    if (nums[i] > 0) return arr
+    while (l < r) {
+      if (nums[l] == nums[i]) l++;
+      if (nums[r] == nums[i]) r--;
+      if (nums[i] + nums[l] + nums[r] == 0) {
+        arr.push([nums[i], nums[l], nums[r]])
+        l++;
+        r--;
+      } else if (nums[i] + nums[l] + nums[r] > 0) {
+        r--;
+      } else if (nums[i] + nums[l] + nums[r] < 0) {
+        l++;
+      }
+    }
+  }
+  return arr
 };
 // @lc code=end
 
